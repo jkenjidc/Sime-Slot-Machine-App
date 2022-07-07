75179b2a467a78605d59e56ac6dd16244697d6a0
kenji dela cruz <kenji.delacruz@willowtreeapps.com>
2022-07-06 23:10:24 -0400

    first commit

--- slot machine/ContentView.swift
+++ slot machine/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var credit = 1000
+    @State var left = "cherry"
+    @State var middle = "apple"
+    @State var right = "star"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("SwiftUi Slots")
+                .font(.largeTitle)
+                .padding()
+            Spacer()
+            Text("Credits: " + String(credit))
+                .padding()
+            Spacer()
+            HStack{
+                Image(left)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image(middle)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Image(right)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            Spacer()
+            Button(action:  {
+                left = determineSlotImage(Int.random(in: 1...3))
+                middle = determineSlotImage(Int.random(in: 1...3))
+                right = determineSlotImage(Int.random(in: 1...3))
+                if left == middle && middle == right{
+                    credit += 25
+                } else{
+                    credit -= 25
+                }
+            }, label: {
+                Text("Spin")
+                    .foregroundColor(Color.white)
+                    .padding(.vertical, 6.0)
+                    .padding(.horizontal, 18)
+                    .background(Color.red)
+                    .clipShape(Capsule())
+            })
+            Spacer()
+
+        }
+    }
+    func determineSlotImage(_ randomNum: Int) -> String{
+        switch randomNum{
+        case 1:
+            return "apple"
+        case 2:
+            return "star"
+        default:
+            return "cherry"
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portraitUpsideDown)
+        }
     }
 }
