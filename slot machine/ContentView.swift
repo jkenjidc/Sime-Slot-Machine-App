5a37aa65b49313e4821dca3061ac28171ffa5543
kenji dela cruz <kenji.delacruz@willowtreeapps.com>
2022-07-06 21:51:39 -0400

    Initial Commit

---
+++ slot machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slot machine
+//
+//  Created by Justine Kenji Dela Cruz on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slot machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot machine
+//
+//  Created by Justine Kenji Dela Cruz on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
